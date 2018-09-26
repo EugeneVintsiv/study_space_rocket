0c8c442a0dca433646357dd9be624d5cf1a6ed3a
Eugene <eugene.vintsiv@gmail.com>
2018-09-26 19:27:51 +0300

    Initial Commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Eugene on 26.09.18.
+//  Copyright © 2018 Eugene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
